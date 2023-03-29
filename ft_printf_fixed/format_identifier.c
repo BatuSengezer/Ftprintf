24549ae73e8ce2af075b262afe2cfbf376311d96
BatuSengezer <113722698+BatuSengezer@users.noreply.github.com>
2023-03-29 17:50:01 +0200

    memory leaks fixed

---
+++ ft_printf_fixed/format_identifier.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   format_identifier.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsengeze <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/23 21:08:30 by bsengeze          #+#    #+#             */
+/*   Updated: 2023/03/23 21:08:37 by bsengeze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	format_identifier(const char format, va_list args)
+{
+	int	length;
+
+	length = 0;
+	if (format == 'c' || format == '%')
+	{
+		if (format == 'c')
+			length += print_char(va_arg(args, int), format);
+		if (format == '%')
+			length += print_char('%', '%');
+	}
+	if (format == 's')
+		length += print_str(va_arg(args, char *));
+	if (format == 'x' || format == 'X')
+		length += print_hex(va_arg(args, unsigned int), format);
+	if (format == 'p')
+		length += print_ptr(va_arg(args, void *), format);
+	if (format == 'd' || format == 'i')
+		length += print_dec(va_arg(args, int));
+	if (format == 'u')
+		length += print_unsigned_int(va_arg(args, unsigned int));
+	return (length);
+}
