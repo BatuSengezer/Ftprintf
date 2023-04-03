c7c9386e4e4e5c005a4616b39ceb95feeff1a2ad
BatuSengezer <113722698+BatuSengezer@users.noreply.github.com>
2023-04-03 17:40:54 +0200

    all done

---
+++ ft_printf.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsengeze <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 15:15:21 by bsengeze          #+#    #+#             */
+/*   Updated: 2023/03/21 18:04:40 by bsengeze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *str, ...)
+{
+	int		print_length;
+	va_list	args;
+
+	print_length = 0;
+	va_start(args, str);
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			if (*str)
+				print_length += format_identifier(*str, args);
+		}
+		else
+		{
+			ft_putchar(*str);
+			print_length++;
+		}
+		str++;
+	}
+	va_end(args);
+	return (print_length);
+}
+/*
+int main (void)
+{
+    int c = 49;
+    char *str = "ABC";
+    int hex = 479;
+    void *ptr = &hex;
+    unsigned int ui = 4294967295;
+
+    ft_printf("%c %% %s  %x  %X \n",c,str,hex,hex);
+    printf("%c %% %s  %x  %X \n",c,str,hex,hex);
+	ft_printf("%p  %d  %d  %i  %u\n",ptr,*(int*)ptr,c,c,ui);
+    printf("%p  %d  %d  %i  %u\n",ptr,*(int*)ptr,c,c,ui);
+    printf(" NULL %s NULL \n", NULL);
+    ft_printf(" NULL %s NULL ", NULL);
+    printf("\n %i",hex_length(hex));
+	printf(" %p\n", -1);
+	ft_printf(" %p\n", -1);
+	ft_printf(" %d ",hex_length(hex));
+	ft_printf(" %x ",hex);
+	ft_printf(" %p ",hex);
+	ft_printf(" %x hex length is: %d\n", 0, hex_length(hex));
+	printf(" %x \n", LONG_MAX);
+	ft_printf(" %x %d\n", LONG_MAX, hex_length(LONG_MAX));
+	printf(" %p %p \n", LONG_MIN, LONG_MAX);
+	ft_printf(" %p %p \n", LONG_MIN, LONG_MAX);
+	ft_printf(0);
+	printf(0);
+}
+*/
